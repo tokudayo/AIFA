404b47fd405e2bdd8989319b1e7a2c847161be2f
NHAT PHAM <violet.eqz@gmail.com>
2022-09-20 16:16:17 +0700

    Added Webcam support; BlazePose implemented and LiteHRNet partly implemented.

--- ai/input.py
+++ ai/input.py
@@ -3,6 +3,9 @@ from .base import InputStream
 
 
 class Cv2VideoStream(InputStream):
+    '''
+    Process video streams in a separate thread.
+    '''
     def __init__(self, video_path, maxsize=30):
         super().__init__(maxsize=maxsize)
         self.video_path = video_path
@@ -15,3 +18,25 @@ class Cv2VideoStream(InputStream):
                 break
             self.queue.put(frame)
         cap.release()
+
+
+
+class WebcamStream(InputStream):
+    '''
+    Process webcam stream.
+    '''
+    def __init__(self, max_fps=30, maxsize=30):
+        super().__init__(maxsize=maxsize)
+        self.max_fps = max_fps
+        self.cap = cv2.VideoCapture(0)
+        self.w = int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.h = int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    def run(self):
+        while self.cap.isOpened():
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+            self.queue.put(frame)
+    
+    def release_cam(self):
+        self.cap.release()
---
+++ ai/models.py
@@ -0,0 +1,76 @@
+from .base.hpe import HpeModel
+import mediapipe as mp
+from LiteHRNet import LiteHRNet
+class BlazePose(HpeModel):
+    '''Google's BlazePose model is a 3D HPE model that can detect 33 landmarks.'''
+    def __init__(self,complexity=1):
+        self.model = mp.solutions.pose.Pose(
+                    static_image_mode=True,
+                    model_complexity=complexity,
+                    min_detection_confidence=0.5)
+    def forward(self, frame):
+    
+        pred = self.model.process(frame)
+        if pred.pose_landmarks:
+            #results = self.to_17_landmarks(pred.pose_landmarks)
+            results = pred.pose_landmarks
+        else :
+            results = None
+        return results
+
+    def to_17_landmarks(self, results, z_plane=False):
+        '''
+        Convert to 17 landmarks.
+        '''
+        raise NotImplementedError
+
+    def predict(self, frame):
+        return self.forward(frame)
+    
+    def __call__(self, frame):
+        return self.predict(frame)
+    
+    def __repr__(self):
+        return "BlazePose"
+
+
+class LiteHRNet(HpeModel):
+    '''LiteHRNet is a 2D HPE model that can detect 17 landmarks.'''
+    def __init__(self):
+        # LiteHRNet-18 implementation
+        base_channel = 16
+        cfg=dict(
+                stem=dict(stem_channels=32, out_channels=32, expand_ratio=1),
+                num_stages=3,
+                stages_spec=dict(
+                    num_modules=(2, 4, 2),
+                    num_branches=(2, 3, 4),
+                    num_blocks=(2, 2, 2),
+                    module_type=('LITE', 'LITE', 'LITE'),
+                    with_fuse=(True, True, True),
+                    reduce_ratios=(8, 8, 8),
+                    num_channels=(
+                        (base_channel, base_channel*2),
+                        (base_channel, base_channel*2, base_channel*4),
+                        (base_channel, base_channel*2, base_channel*4, base_channel*8),
+                    )),
+            )
+        self.model = LiteHRNet(cfg, in_channels=3)
+        #self.model.load_state_dict(torch.load('LiteHRNet.pth'))
+        self.model.eval()
+
+    def forward(self, frame):
+    
+        pred = self.model.process(frame)
+        if pred.pose_landmarks:
+            #results = self.to_17_landmarks(pred.pose_landmarks)
+            results = pred.pose_landmarks
+        else :
+            results = None
+        return results
+
+    def predict(self, frame):
+        return self.forward(frame)
+    
+    def __repr__(self):
+        return "LiteHRNet"
