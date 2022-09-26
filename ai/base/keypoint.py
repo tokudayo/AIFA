874415ceda223cfe9861a918b7e3ef9ffc5f624a
Đức Tô <20toduc01@gmail.com>
2022-09-26 16:46:57 +0700

    Stuff

---
+++ ai/base/keypoint.py
@@ -0,0 +1,30 @@
+from typing import Optional
+
+import numpy as np
+
+
+class KeyPoint(object):
+    def __init__(self, name: Optional[str] = None, memory: int = 5):
+        self.name = name
+        self.memory = memory
+        self.pos = np.zeros((memory, 3))
+    
+    def update(self, x, y, z):
+        self.pos = np.roll(self.pos, 1, axis=0)
+        self.pos[0] = np.array([x, y, z])
+
+    @property
+    def velocity(self):
+        return self.pos[0] - self.pos[1]
+    
+    @property
+    def avg_velocity(self):
+        return np.mean(self.pos[1:] - self.pos[:-1], axis=0)
+    
+    @property
+    def acceleration(self):
+        return self.velocity - self.avg_velocity
+    
+    @property
+    def avg_acceleration(self):
+        return np.mean(self.pos[2:] - 2*self.pos[1:-1] + self.pos[:-2], axis=0)
---
+++ ai/exercises/shoulder_press.py
@@ -0,0 +1,50 @@
+from ai.base.keypoint import KeyPoint
+
+
+kps_anno = {
+    0: 'nose',
+    1: 'left_eye',
+    2: 'right_eye',
+    3: 'left_ear',
+    4: 'right_ear',
+    5: 'left_shoulder',
+    6: 'right_shoulder',
+    7: 'left_elbow',
+    8: 'right_elbow',
+    9: 'left_wrist',
+    10: 'right_wrist',
+    11: 'left_hip',
+    12: 'right_hip',
+    13: 'left_knee',
+    14: 'right_knee',
+    15: 'left_ankle',
+    16: 'right_ankle',
+}
+
+class ShoulderPress():
+    """
+    Needs to:
+    - Somehow detect what part of exercise is being performed by:
+    + Tracking position, velocity, acceleration of keypoints. Downside is if the HPE model is unstable, this will be too.
+    + 
+    - Evaluation for that part of exercise
+    - Return a score for that part of exercise"""
+    def __init__(self, memory=5):
+        self.kps = {}
+        for name in kps_anno.values():
+            self.kps[name] = KeyPoint(name, memory)
+
+    def update(self, kps):
+        """Pass normalized keypoints info in and update internal state"""
+        for i, kp in enumerate(kps):
+            self.kps[kps_anno[i]].update(kp)
+
+    @property
+    def state(self):
+        kps = self.kps
+        if kps['left_elbow'].avg_velocity > -0.1:
+            return 'up'
+        elif kps['left_elbow'].avg_velocity < 0.1:
+            return 'down'
+        else:
+            return 'none'
