add27306baf829d021e6880069803c2436914325
Đức Tô <20toduc01@gmail.com>
2022-09-29 19:18:56 +0700

    More details

--- ai/exercises/shoulder_press.py
+++ ai/exercises/shoulder_press.py
@@ -3,6 +3,7 @@ from typing import List, Tuple
 
 from ai.base.exercise import Exercise
 from ai.base.vector import xaxis
+from ai.exercises.utils import deg_to_rad, rad_to_deg
 
 
 class ShoulderPress(Exercise):
@@ -21,19 +22,23 @@ class ShoulderPress(Exercise):
         #     f"Current state: {self.state}, previous state: {self.prev_state}"
         # )
         # 4 cases:
-        error = 0.15
+        allowed_error = deg_to_rad(10*2)
         left_arm = self.joint_vector('left_wrist', 'left_elbow')
         right_arm = self.joint_vector('right_wrist', 'right_elbow')
-        if state == 'down' and self.prev_state == 'up':
-            pass
-        elif state == 'up' and self.prev_state == 'down':
-            pass
-        elif state == self.prev_state and state in ['up', 'down']:
+        print(f"STATE: {state}")
+        if state == self.prev_state and state in ['up', 'down']:
             print("judging")
-            if abs(left_arm.angle(xaxis) - math.pi/2) > error or abs(
-                    right_arm.angle(xaxis) - math.pi/2) > error:
+            if abs(left_arm.angle(xaxis) - math.pi/2) > allowed_error or abs(
+                    right_arm.angle(xaxis) - math.pi/2) > allowed_error:
                 # self.msg_queue.put('U shud keep your arms straight')
                 print("gay arm")
+        elif state != self.prev_state:
+            bl_arm = self.joint_vector('left_elbow', 'left_shoulder')
+            br_arm = self.joint_vector('right_elbow', 'right_shoulder')
+            if abs(bl_arm.angle(xaxis) - math.pi/2) > allowed_error or abs(
+                    br_arm.angle(xaxis) - math.pi/2) > allowed_error:
+                # self.msg_queue.put('U shud keep your arms straight')
+                print(f"harder arm, angle={rad_to_deg(bl_arm.angle(xaxis))}")
 
     @property
     def state(self):
---
+++ ai/exercises/utils.py
@@ -0,0 +1,9 @@
+import math
+
+
+def deg_to_rad(deg):
+    return deg * math.pi / 180
+
+
+def rad_to_deg(rad):
+    return rad * 180 / math.pi
