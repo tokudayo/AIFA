6dfe2c9f691e768003fc02d33526b13b68323209
Đức Tô <20toduc01@gmail.com>
2022-10-05 12:36:49 +0700

    Checkpoint

--- ai/base/pose.py
+++ ai/base/pose.py
@@ -1,24 +1,15 @@
-from typing import List
+from typing import List, Optional
 import numpy as np
 
 from ai.base.vector import Vector
 from ai.models.anno import coco_anno_list as kps_anno
 
 
-skeletal_angle_template = [
-    [5, 6],
-    [6, 8],
-    [5, 7],
-    [8, 10],
-    [7, 9],
-]
-
-
 class Pose(object):
     def __init__(self, kps: List[List[float]]):
         self.data = np.array(kps)
         # Hip length is used to normalize the pose
-        self.norm_factor = np.linalg.norm(self.data[11] - self.data[12])
+        # self.norm_factor = np.linalg.norm(self.data[11] - self.data[12])
 
     def __getitem__(self, key: str):
         return self.data[kps_anno.index(key)]
@@ -27,48 +18,49 @@ class Pose(object):
         return Vector(self[key1][..., :3], self[key2][..., :3])
 
     def similarity(self, other: 'Pose', template: str = 'skeletal_angle'):
-        if template == 'skeletal_angle':
-            return self.skeletal_angle_similarity(other)
-        else:
-            raise NotImplementedError
-
-    def skeletal_angle_similarity(self, other: 'Pose'):
-        """
-        Calculate the similarity between two poses based on skeletal angle.
-        """
-        # Normalize the pose
-        self_norm = self.data / self.norm_factor
-        other_norm = other.data / other.norm_factor
-
-        # Calculate the skeletal angle
-        self_angle = self.skeletal_angle()
-        other_angle = other.skeletal_angle()
-
-        # Calculate the similarity
-        return np.mean(np.abs(self_angle - other_angle))
+        raise NotImplementedError
 
     def __repr__(self):
         return f'Pose({self.data})'
 
 
 class PoseSeries(object):
-    def __init__(self, poses: List[Pose]):
+    def __init__(self, poses: Optional[List[Pose]] = None):
+        if poses is None:
+            self.data = None
         if isinstance(poses, np.ndarray):
             self.data = poses
         elif isinstance(poses, list):
             if isinstance(poses[0], Pose):
                 self.data = np.array([pose.data for pose in poses])
-            elif isinstance(poses[0], np.ndarray):
-                self.data = poses
+            else:
+                self.data = np.vstack(np.array(poses))
 
     def __getitem__(self, slice):
         return self.data[slice]
 
-    def kp_series(self, key: str):
-        return self.data[:, kps_anno.index(key)]
+    def kp_series(self, *key: List[str]):
+        return PoseSeries(
+            self.data[:, [kps_anno.index(k) for k in key], ...]
+        )
 
-    def kp_vector_series(self, key1: str, key2: str):
-        return Vector(self.kp_series(key2)[..., :3], self.kp_series(key1)[..., :3])
+    def joint_vector_series(self, key1: str, key2: str):
+        return Vector(
+            self.data[:, kps_anno.index(key1)][..., :3],
+            self.data[:, kps_anno.index(key2)][..., :3]
+        )
+
+    @property
+    def displacement(self):
+        return self.data[-1] - self.data[0]
 
     def update(self, pose: Pose):
-        self.data = np.append(self.data, pose.data)
+        if self.data is None:
+            if pose.data.ndim < 3:
+                self.data = np.array(pose.data)[None, ...]
+            else:
+                self.data = np.array(pose.data)
+        else:
+            self.data = np.concatenate(
+                [self.data, pose.data[None, ...]], axis=0
+            )
